efe40c7450284a8d2d8fc9bea89c19746e2e8cd2
supermanshuvo <shuvothesuperman@gmail.com>
2021-06-21 00:08:46 +0600

    Create nav in nav jsx and link to the home. add some h1 and div tag to create layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,16 @@
+import Nav from './Nav'
 const Home = () =>{
     return(
-        <div>Hello From Home component</div>
+        <div>
+            <Nav />
+            <div className="container">
+                <div className="row text-center">
+                    <h1 className="mt-5 text-info">Welcome to The Basic Calculator</h1>
+                    <div className="col-md-6"></div>
+                    <div className="col-md-6"></div>
+                </div>
+            </div>
+        </div>
     );
 }
 export default Home;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+    return (
+        <nav className="navbar navbar-light bg-primary">
+            <div className="container-fluid">
+                <div className="container">
+                    <span className="navbar-brand mb-0 h1 text-white">Basic Calculator</span>
+                </div>
+            </div>
+        </nav>
+    );
+}
+export default Nav;
