800e16b04db313deeaba040649722bdbdf891720
supermanshuvo <shuvothesuperman@gmail.com>
2021-06-22 00:31:33 +0600

    create diffreant function for add

---
+++ src/components/Add.jsx
@@ -0,0 +1,6 @@
+const Add = ()=>{
+    return(
+        
+    );
+}
+export default Add;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,22 +4,47 @@ class Home extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            number1:'',
-            number2:'',
-            add:'',
-            sub:'',
-            mul:'',
-            div:''
+            firstNumber:'',
+            secondNumber:''
         };
-        this.handleChange = this.handleChange.bind(this);
     };
-    handleChange(event){
-        this.setState({
-            number1: event.target.number1,
-            number2: event.target.number2,
-        })
+    handleChange=(event)=>{
+        var inputName = event.target.name;
+        var inputValue = event.target.value;
+        this.setState({[inputName]:inputValue});
     }
-
+    add=()=>{
+        let num1 = parseFloat(this.state.firstNumber);
+        let num2 = parseFloat(this.state.secondNumber);
+        let resutl = num1 + num2;
+        let added = resutl.toFixed(2);
+    }
+    sub=()=>{
+        let num1 = parseFloat(this.state.firstNumber);
+        let num2 = parseFloat(this.state.secondNumber);
+        let resutl = num1 - num2;
+        let subbed = resutl.toFixed(2);
+    }
+    multi=()=>{
+        let num1 = parseFloat(this.state.firstNumber);
+        let num2 = parseFloat(this.state.secondNumber);
+        if(isNaN(num2) || num2<1){
+            let mult = '';
+        }else{
+            let resutl = num1 * num2;
+            let mult = resutl.toFixed(2);
+        }
+    }
+    div=()=>{
+        let num1 = parseFloat(this.state.firstNumber);
+        let num2 = parseFloat(this.state.secondNumber);
+        if(isNaN(num2) || num2<1){
+            let div = '';
+        }else{
+            let resutl = num1 / num2;
+            let div = resutl.toFixed(2);
+        }
+    }
     render() {
         return(
             <div>
@@ -28,16 +53,18 @@ class Home extends React.Component {
                     <div className="row">
                         <h1 className="mt-5 text-center text-info">Welcome to The Basic Calculator</h1>
                         <div className="col-md-6">
-                            <h4>Enter your value</h4>
-                            <label for="inputNumber" className="form-label">Enter First Number:</label>
-                            <input type="number"value={this.state.number1} onChange={this.handleChange} className="form-control" name="firstNumber" id="firstNumber" placeholder="Enter your number"/>
-                            <label for="inputNumber" className="form-label">Enter Second Number:</label>
-                            <input type="number"value={this.state.number2} onChange={this.handleChange} className="form-control" name="secondNumber" id="secondNumber" placeholder="Enter your number"/>
+                            <form>
+                                <h4>Enter your value</h4>
+                                <label htmlFor="inputNumber" className="form-label">Enter First Number:</label>
+                                <input type="number" value={this.state.number1} onChange={this.handleChange} className="form-control" name="firstNumber" id="firstNumber" placeholder="Enter your number"/>
+                                <label htmlFor="inputNumber" className="form-label">Enter Second Number:</label>
+                                <input type="number" value={this.state.number2} onChange={this.handleChange} className="form-control" name="secondNumber" id="secondNumber" placeholder="Enter your number"/>
+                            </form>
                         </div>
                         <div className="col-md-6">
                             <h4 className="mb-4">Result</h4>
                             <p>Addition : <span></span></p>
-                            <p>Subtraction : <span></span></p>
+                            <p>Subtraction : <span>{ this.state.secondNumber }</span></p>
                             <p>Multiply : <span></span></p>
                             <p>Division : <span></span></p>
                         </div>
