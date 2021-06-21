0c25027d9277d40ed4c4e47347f68b60051a39bf
supermanshuvo <shuvothesuperman@gmail.com>
2021-06-21 21:27:20 +0600

    add event handle on change and add value to the input box

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import Nav from './Nav'
 class Home extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
+            number1:'',
+            number2:'',
             add:'',
             sub:'',
             mul:'',
             div:''
-        }
+        };
+        this.handleChange = this.handleChange.bind(this);
     };
+    handleChange(event){
+        this.setState({
+            number1: event.target.number1,
+            number2: event.target.number2,
+        })
+    }
+
     render() {
         return(
             <div>
@@ -20,9 +30,9 @@ class Home extends React.Component {
                         <div className="col-md-6">
                             <h4>Enter your value</h4>
                             <label for="inputNumber" className="form-label">Enter First Number:</label>
-                            <input type="number" className="form-control" name="firstNumber" id="firstNumber" placeholder="Enter your number"/>
+                            <input type="number"value={this.state.number1} onChange={this.handleChange} className="form-control" name="firstNumber" id="firstNumber" placeholder="Enter your number"/>
                             <label for="inputNumber" className="form-label">Enter Second Number:</label>
-                            <input type="number" className="form-control" name="secondNumber" id="secondNumber" placeholder="Enter your number"/>
+                            <input type="number"value={this.state.number2} onChange={this.handleChange} className="form-control" name="secondNumber" id="secondNumber" placeholder="Enter your number"/>
                         </div>
                         <div className="col-md-6">
                             <h4 className="mb-4">Result</h4>
